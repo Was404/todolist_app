d791608cc135b79ed5964b78d6131a118366aa74
Was404 <vladimirovichdanil@bk.ru>
2024-08-06 18:47:34 +0300

    asinc def + router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 from database import SessionLocal, engine, Base
+from routers import user as UserRouter
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+app.include_router(UserRouter.router, prefix="/user")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True, workers=3)
-    
