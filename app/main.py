d530db04f09ac13101099e59ac3336a4c9fd06cc
Was404 <vladimirovichdanil@bk.ru>
2024-08-06 16:42:45 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,8 @@
+import uvicorn
+from fastapi import FastAPI
+from database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True, workers=3)
