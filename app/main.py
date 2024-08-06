c9a0cf728bf82279c14789adefbe2b2d0864d040
Was404 <vladimirovichdanil@bk.ru>
2024-08-06 17:57:12 +0300

    add model user

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from database import SessionLocal, engine, Base
 
 Base.metadata.create_all(bind=engine)
+app = FastAPI()
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True, workers=3)
+    
---
+++ models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
