e1077aa3e5a585de4c765a355a97bc542237d405
Was404 <wqerasmr@mail.ru>
2024-08-16 00:43:04 +0300

    add env for log.ini

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from .routers import users, tasks
 
-app = FastAPI()
+app = FastAPI(
+    tittle="ToDo List App"
+)
 
-app.include_router(users.router)
-app.include_router(tasks.router)
+app.include_router(users.router, tags=["Users"])
+app.include_router(tasks.router, tags=["Tasks"])
 
-# uvicorn example:app --log-config /path/to/log.ini
+# uvicorn app.main:app --log-config /env/log.ini
