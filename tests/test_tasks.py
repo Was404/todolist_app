aba5ee2dc8b473a250fc291955fda995537c5266
Was404 <wqerasmr@mail.ru>
2024-08-21 21:35:59 +0300

    new tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,43 +1,72 @@
 from app.main import app  # добавляем наше приложение
-from conftest import TestingSessionLocal  # добавляем тестовую бд
+from .conftest import TestingSessionLocal, init_db
 from fastapi.testclient import TestClient
 import pytest
-import app.crud as crud
 
-#Base.metadata.drop_all(bind=engine)  # Удаляем таблицы после тестов
-#Base.metadata.create_all(bind=engine)  # Восстанавливаем таблицы
+"""
+Описание тестов:
+
+- test_create_task: Создание тестовой задачи.
+- test_read_task: Проверяет возможность чтения информации о задаче.
+- test_update_task: Проверяет возможность обновления информации задачи.
+- test_delete_task: Проверяет возможность удаления задачи
+     и подтверждает, что задача удалена.
+"""
 
 
-def get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@pytest.fixture(scope="module")
+def client():
+    # Инициализация тестовой базы данных
+    init_db()
+    # Создание клиентского объекта
+    client = TestClient(app)
+    yield client
+
+    # добавить очистку базы данных TODO
+
+
+@pytest.fixture(scope="module")
+def db_session():
+    # Настройка сессии для тестов
+    connection = TestingSessionLocal()
+    yield connection
+    connection.close()
+
+
+task_data = {"title": "TEST TASK", 
+             "description": "delete me", 
+             "completed": "false"}
 
 
 @pytest.fixture
-def client():  # Новый клиент
-    def _get_test_db_override():
-        db = TestingSessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
-    app.dependency_overrides[get_db] = _get_test_db_override
-    return TestClient(app)
+def create_task(client, db_session):
+    response = client.post("/tasks/", json=task_data)
+    return response.json()
 
 
-def test_tasks(client):
-    response = client.get("/tasks/")
+def test_create_task(client, db_session):
+    response = client.post("/tasks/", json=task_data)
     assert response.status_code == 200
-    #  assert response.json() == [{}]
+    assert response.json()["title"] == "TEST TASK"
+   
 
-
-def test_create_task(client):  # TODO как обратиться -> crud.create_task(db = ...)
-    """Метод создает задачу для одного пользователя"""
-    client.post("/tasks/", json={"name": "Test Item"})
-    response = client.get("/items/")
+def test_read_task(client, db_session):
+    response = client.get("/tasks/1")
     assert response.status_code == 200
-    assert len(response.json()) > 0
-    # assert crud.create_task()
+    # assert response.json() == create_user
+
+
+def test_update_task(client, db_session):
+    response = client.put("/tasks/1", json={"title": "TEST TASK", 
+                                            "description": "delete me", 
+                                            "completed": "True"})
+    assert response.status_code == 200
+    assert response.json()["completed"] == "true"
+
+
+def test_delete_task(client, db_session):
+    response = client.delete("/tasks/1")
+    assert response.status_code == 200
+
+    response = client.get("/tasks/1")  # проверка что user удалился
+    assert response.status_code == 404
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,14 +1,74 @@
 from app.main import app  # добавляем наше приложение
+from .conftest import TestingSessionLocal, init_db
 from fastapi.testclient import TestClient
 import pytest
 
+"""
+Описание тестов:
+
+- test_create_user: Создание тестового пользователя.
+- test_read_user: Проверяет возможность чтения информации о пользователе.
+- test_update_user: Проверяет возможность обновления информации о пользователе.
+- test_delete_user: Проверяет возможность удаления пользователя
+     и подтверждает, что он был удалён.
+"""
+
+
+@pytest.fixture(scope="module")
+def client():
+    # Инициализация тестовой базы данных
+    init_db()
+    # Создание клиентского объекта
+    client = TestClient(app)
+    yield client
+
+    # добавить очистку базы данных TODO
+
+
+@pytest.fixture(scope="module")
+def db_session():
+    # Настройка сессии для тестов
+    connection = TestingSessionLocal()
+    yield connection
+    connection.close()
+
+
+user_data = {"username": "TestUser", 
+             "email": "test@gmail", 
+             "full_name": "Tester Testerson", 
+             "password": "123test"}
+
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def create_user(client, db_session):
+    response = client.post("/users/", json=user_data)
+    return response.json()
 
 
-def test_get_user(client):
-    response = client.get("/users/")
+def test_create_user(client, db_session):
+    response = client.post("/users/", json=user_data)
     assert response.status_code == 200
-    #  assert response.json() == [{}]
+    assert response.json()["username"] == "TestUser"
+  
+
+def test_read_user(client, db_session):
+    response = client.get("/users/1")
+    assert response.status_code == 200
+    # assert response.json() == create_user
+
+
+def test_update_user(client, db_session):
+    response = client.put("/users/1", json={"username": "string", 
+                                            "email": "string", 
+                                            "full_name": "string", 
+                                            "password": "string"})
+    assert response.status_code == 200
+    assert response.json()["username"] == "string"
+
+
+def test_delete_user(client, db_session):
+    response = client.delete("/users/1")
+    assert response.status_code == 200
+
+    response = client.get("/users/1")  # проверка что user удалился
+    assert response.status_code == 404
